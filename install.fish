e606ec072ce77d630345384452eebd21e8d9d141
Trygve <177544878+th926@users.noreply.github.com>
2026-03-25 15:35:33 +0100

    Created helper scripts

---
+++ install.fish
@@ -0,0 +1,18 @@
+source env.fish
+if test -d $target_dir
+    echo "Directory $target_dir already exists.\n";
+    while true
+        read "-l -p Want to wipe and install? (y/N)" confirm
+        if [$confirm = "y"]
+            rm -rf "$target_dir/*"
+        else
+            echo "exiting"
+            return
+        end
+    end
+end
+
+mkdir -p "$target_dir/template"
+cp "template/* $target_dir/template"
+cp "$bin_name $target_dir"
+ln "-sf $target_dir/$bin_name $link_loc"
