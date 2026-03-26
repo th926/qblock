7285aef15459558d2bdc75453195d59abfdace49
Trygve <177544878+th926@users.noreply.github.com>
2026-03-26 15:57:42 +0100

    fixed install script

--- env.fish
+++ env.fish
@@ -1,3 +1,4 @@
 set -x bin_name "qblock"
 set -x target_dir "/Users/$USER/.config/$bin_name"
-set -x link_loc "usr/bin/$bin_name"
+set -x link_dir "/usr/local/bin/"
+set -x link_loc "/usr/local/bin/$bin_name"
--- install.fish
+++ install.fish
@@ -1,18 +1,21 @@
 source env.fish
 if test -d $target_dir
     echo "Directory $target_dir already exists.\n";
-    while true
-        read "-l -p Want to wipe and install? (y/N)" confirm
-        if [$confirm = "y"]
-            rm -rf "$target_dir/*"
-        else
-            echo "exiting"
-            return
-        end
+    read -lP "Want to wipe and install? (y/N)" confirm
+    if test $confirm = "y"
+        rm -rf "$target_dir/*"
+    else
+        echo "exiting"
+        return
     end
 end
 
 mkdir -p "$target_dir/template"
-cp "template/*" "$target_dir/template"
+cp -r template/* "$target_dir/template"
 cp "$bin_name" "$target_dir"
-ln "-sf $target_dir/$bin_name" "$link_loc"
+if !test -d $link_dir
+    mkdir -p $link_dir
+end
+ln -sf "$target_dir/$bin_name" "$link_loc"
+set -U fish_user_paths "/usr/local/bin"
+echo "/usr/local/bin has been added to path, just so you know"
