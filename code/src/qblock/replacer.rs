c311d5697568676396824ea87281d2dd620e72f2
Trygve <177544878+th926@users.noreply.github.com>
2026-03-27 13:27:05 +0100

    split structs into files

---
+++ code/src/qblock/replacer.rs
@@ -0,0 +1,62 @@
+use std::path::PathBuf;
+use std::fs;
+
+use crate::qblock::{wpinstall::WPinstall, block::Block, template::Template};
+
+
+pub struct ReplacerPlaceholders {
+    cap_pattern: String,
+    low_pattern: String,
+}
+
+impl ReplacerPlaceholders {
+    pub fn new(cap: &str, low: &str) -> Self {
+        Self {
+            cap_pattern: String::from(cap),
+            low_pattern: String::from(low),
+        }
+    }
+}
+
+pub struct Replacer {
+    placeholders: ReplacerPlaceholders,
+    wp: WPinstall,
+}
+
+impl Replacer {
+    pub fn new(in_wp: WPinstall, in_placeholders: ReplacerPlaceholders) -> Self {
+        Self {
+            placeholders: in_placeholders,
+            wp: in_wp,
+        }
+    }
+    pub fn template_to_block(&self, template: Template, template_path: &PathBuf, block: &Block) {
+        let template_full_path = self.wp.full_path.join(&template.location); // Change template
+        let mut template_content = match fs::read_to_string(&template_full_path) {
+            Ok(res) => res,
+            Err(e) => panic!("Failed reading file: {}, Error: {}", &template_full_path.display(), e),
+        };
+        if template.low {
+            template_content = template_content.replace(&self.placeholders.low_pattern, &block.name);
+        }
+        if template.cap {
+            template_content = template_content.replace(&self.placeholders.cap_pattern, &block.to_upper());
+        }
+        let block_full_path: PathBuf;
+        if template.is_assets {
+            block_full_path = self.wp.full_path.join(&block.to_path()).join(template.namechange.unwrap());
+        } else {
+            block_full_path = self.wp.full_path.join(&block.to_path()).join(template.location);
+        }
+        match fs::write(&block_full_path, template_content) {
+            Ok(_) => (),
+            Err(e) => {
+                match fs::remove_dir_all(&block.to_path()){
+                    Ok(_) => (),
+                    Err(e_i) => panic!("Failed failing cleanup had errors: {}", e_i),
+                }
+                panic!("Failed writing to file: {}, Error: {}", &block_full_path.display(), e);
+            },
+        };
+    }
+}
